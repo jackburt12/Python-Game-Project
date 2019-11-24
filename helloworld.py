ab0781f69de856d733f074aa53b96a8790ade128
jackburt12 <contact@jackburt.me.uk>
2019-11-24 17:51:03 +0000

    Flask test

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,7 @@
-print("Hello world")
-print("Changes")
-print("professional")
-print("hello")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
