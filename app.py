c7387bb7032198f2b06ab83124cec707928c06a0
jackburt12 <contact@jackburt.me.uk>
2019-11-25 14:38:49 +0000

    Working inventory and statistics

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
+from items import GetItem
 from flask import Flask, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///player.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Player(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), nullable=False)
+    health = db.Column(db.Integer, default=100)
+    hunger = db.Column(db.Integer, default=100)
+    energy = db.Column(db.Integer, default=100)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.String(32), nullable=False)
+    item_type = db.Column(db.String(8), nullable=False)
+    quantity = db.Column(db.Integer, default=1)
 
     def __repr__(self):
         return '<Task %r>' % self.id
@@ -19,8 +32,7 @@ def index():
     if request.method=='POST':
         if 'new_game' in request.form:
             try:
-                db.session.delete(Player.query.first())
-                db.session.commit()
+                clear_database()
                 return render_template("create-character.html")
             except:
                 return 'There was an issue starting a new game!'
@@ -28,10 +40,19 @@ def index():
             player_name = request.form['name']
             player = Player(name=player_name)
 
+            #this is how you add an item to the player
+            #you must lookup the item you wish to add in its relevant csv file
+            #whichever line it is in the csv file is what the item_id should be
+            swiss = Item(item_id=1, item_type="Weapon")
+            nuts = Item(item_id=1, item_type="Consumable")
+            
             try:
+                clear_database()
                 db.session.add(player)
+                db.session.add(swiss)
+                db.session.add(nuts)
                 db.session.commit()
-                return render_template("game-interface.html", player=player)
+                return render_template("game-interface.html", player=player, items=load_inventory())
             except:
                 return 'There was an issue creating the player'
     else:
@@ -39,7 +60,22 @@ def index():
         if current_player is None:
             return render_template("create-character.html")
         else:
-            return render_template("game-interface.html", player=current_player)
+            return render_template("game-interface.html", player=current_player, items=load_inventory())
+
+def load_inventory():
+    items_raw = Item.query.all()
+    items_parsed = []
+    for item in items_raw:
+        new_item = GetItem(item.item_type, item.item_id)
+        new_item.quantity = item.quantity
+        items_parsed.append(new_item)
+
+    return items_parsed
+
+def clear_database():
+    db.session.query(Item).delete()
+    db.session.query(Player).delete()
+    db.session.commit()
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ items.py
@@ -0,0 +1,62 @@
+import csv
+
+class Item():
+    """The base class for all items"""
+
+    quantity = 0
+
+    def __init__(self, name, description, value):
+        self.name = name
+        self.description = description
+        self.value = value
+        quantity = 1
+
+class Weapon(Item):
+    def __init__(self, name, description, value, damage):
+        self.damage = damage
+        super().__init__(name, description, value)
+
+class Armour(Item):
+    def __init__(self, name, description, value, protection):
+        self.protection = protection
+        super().__init__(name, description, value)
+
+class Consumable(Item):
+    def __init__(self, name, description, value, effect, amount):
+        self.effect = effect
+        self.amount = amount
+        super().__init__(name, description, value)
+
+def GetItem(item_type, item_id):
+    file = ""
+    if item_type == "Weapon":
+        file = "resources/weapons.csv"
+    elif item_type == "Armour":
+        file = "resources/armour.csv"
+    elif item_type == "Consumable":
+        file = "resources/consumables.csv"
+    elif item_type == "Material":
+        file = "resources/materials.csv"
+
+
+    with open(file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            name = row[0]
+            print(name, flush=True)
+
+            description = row[1]
+            value = row[2]
+
+            if item_type == "Weapon":
+                damage = row[3]
+                return Weapon(name, description, value, damage)
+            elif item_type == "Armour":
+                protection = row[3]
+                return Armour(name, description, value, protection)
+
+            elif item_type == "Consumable":
+                effect = row[3]
+                amount = row[4]
+                return Weapon(name, description, effect, amount)
